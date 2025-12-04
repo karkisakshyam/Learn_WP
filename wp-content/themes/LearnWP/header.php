c5515e752fc2e536affdcdfdb4cb31c0bc4ea00f
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-04 07:31:38 +0545

    learning about WP

---
+++ wp-content/themes/LearnWP/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+</head>
+
+<body>
---
+++ wp-content/themes/LearnWP/index.php
@@ -0,0 +1,5 @@
+<?php get_header()?>
+    <h1>Welcome to the new series of learning WordPress</h1>
+    <h1>I am Sakshyam Karki.</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illo eius dignissimos consectetur iusto tempore expedita quibusdam mollitia commodi a, tempora ut illum possimus sunt aspernatur in. Autem, natus commodi?</h2>
+<?php get_footer()?>

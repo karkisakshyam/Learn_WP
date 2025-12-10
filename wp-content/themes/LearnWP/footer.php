c755141830f56815bd2879ab8e80acbf1ffcf02b
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-10 07:42:40 +0545

    Wordpress

--- wp-content/themes/LearnWP/footer.php
+++ wp-content/themes/LearnWP/footer.php
@@ -1,2 +1,19 @@
-</body>
-</html>
+<footer class="flex flex-col gap-6 py-10 text-center border-t border-solid border-gray-200 dark:border-gray-800">
+<div class="flex flex-wrap justify-center gap-4">
+<a class="text-subtle-text-light dark:text-subtle-text-dark hover:text-primary transition-colors" href="#">
+<span class="material-symbols-outlined !text-2xl">alternate_email</span>
+</a>
+<a class="text-subtle-text-light dark:text-subtle-text-dark hover:text-primary transition-colors" href="#">
+<span class="material-symbols-outlined !text-2xl">code</span>
+</a>
+<a class="text-subtle-text-light dark:text-subtle-text-dark hover:text-primary transition-colors" href="#">
+<span class="material-symbols-outlined !text-2xl">palette</span>
+</a>
+</div>
+<p class="text-subtle-text-light dark:text-subtle-text-dark text-base font-normal leading-normal">© 2024 Olivia Chen</p>
+</footer>
+</div>
+</div>
+</div>
+</div>
+</body></html>
---
+++ wp-content/themes/LearnWP/functions.php
@@ -0,0 +1,4 @@
+<?php
+wp_enqueue_style ('tailwinfcss', get_template_directory_uri() . '/src/output.css', [], '1.0.0', 'all');
+wp_enqueue_style('learnwp-style', get_stylesheet_uri(), [], '1.0.1', 'all');
+?>

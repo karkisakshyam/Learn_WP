1f042678fba63fad4fbd58712ef6948b8dbbd98e
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-11 07:48:50 +0545

    wordpress

--- wp-content/themes/LearnWP/functions.php
+++ wp-content/themes/LearnWP/functions.php
@@ -1,4 +1,9 @@
 <?php
+
+function learn_wp_styles(){
 wp_enqueue_style ('tailwinfcss', get_template_directory_uri() . '/src/output.css', [], '1.0.0', 'all');
 wp_enqueue_style('learnwp-style', get_stylesheet_uri(), [], '1.0.1', 'all');
-?>
+
+}
+
+add_action ('wp_enqueue_scripts', 'learn_wp_styles');
